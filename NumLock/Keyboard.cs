47b8a29326912b348f917f2ab58cee8a06f59211
Bruno Leonardo Michels <brunolm7@gmail.com>
2017-05-08 23:32:17 -0300

    Add project files.

---
+++ NumLock/Keyboard.cs
@@ -0,0 +1,30 @@
+﻿namespace NumLock
+{
+    using System.Runtime.InteropServices;
+    using System.Windows.Forms;
+
+    class Keyboard
+    {
+        [DllImport("user32.dll")]
+        internal static extern short GetKeyState(int keyCode);
+
+        [DllImport("user32.dll")]
+#pragma warning disable IDE1006 // Naming Styles
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+#pragma warning restore IDE1006 // Naming Styles
+
+        public static void PressKeyboardButton(Keys keyCode)
+        {
+            const int KEYEVENTF_EXTENDEDKEY = 0x1;
+            const int KEYEVENTF_KEYUP = 0x2;
+
+            keybd_event((byte)keyCode, 0x45, KEYEVENTF_EXTENDEDKEY, 0);
+            keybd_event((byte)keyCode, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+        }
+
+        public static bool CheckState(Keys key)
+        {
+            return GetKeyState((int)key) == 1;
+        }
+    }
+}
---
+++ NumLock/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumLock
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            if (!Keyboard.CheckState(Keys.NumLock))
+            {
+                Keyboard.PressKeyboardButton(Keys.NumLock);
+            }
+        }
+    }
+}
